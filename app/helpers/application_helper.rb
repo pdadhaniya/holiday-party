1eb0c8eb7b3c82846b3f8df1c45c61b83f9d8388
Shreya Patel <shreya317@gmail.com>
2016-11-26 16:52:16 -0600

    updated nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def countdown
-    wedding_day = Date.parse("5/8/2016")
+    wedding_day = Date.parse("23/12/2016")
     today = Date.today
     return (wedding_day - today).to_i
   end
